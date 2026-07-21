584d44b643ed8872d2c6f18e3cafeaaa720d42d9
lmg-toad <toad@louisvillemakesgames.org>
2026-07-21 11:21:54 -0400

    added sneeze and dash req

--- player_movement.gd
+++ player_movement.gd
@@ -4,15 +4,28 @@ extends CharacterBody2D
 @export var friction : float = 0.8
 @export var dashSpeed : int = 2000
 @export var gravity : int = 100
-@export var jumpSpeed : int = 2500
+@export var jumpSpeed : int = 2000
+@export var secondJumpSpeed : int = 2500
+@export var maxRise : int = -3000
+var canSneeze : bool = false
+var canDash : bool = false
 
 func _physics_process(delta: float) -> void:
 	velocity.x += Input.get_axis("move_left","move_right") * playerSpeed
 	velocity.x *= friction
-	if Input.is_action_just_pressed("dash"):
+	if is_on_floor() :
+		canSneeze = true
+		canDash = true
+	if canDash and Input.is_action_just_pressed("dash") :
 		velocity.x+=Input.get_axis("move_left","move_right")*dashSpeed
+		canDash = false
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y=-jumpSpeed
+	if canSneeze and Input.is_action_just_pressed("sneeze") :
+		velocity.y -= secondJumpSpeed
+		canSneeze = false
+	if velocity.y < maxRise :
+		velocity.y = maxRise
 	velocity.y += gravity
 	move_and_slide()
 	
