83a9bdc41d575694f8cd1824c23f20251fe37252
lmg-abe <abe@louisvillemakesgames.org>
2026-07-21 10:47:28 -0400

    jump only on ground

--- player_movement.gd
+++ player_movement.gd
@@ -5,14 +5,13 @@ extends CharacterBody2D
 @export var dashSpeed : int = 2000
 @export var gravity : int = 100
 @export var jumpSpeed : int = 2500
-var grounded : bool = false
 
 func _physics_process(delta: float) -> void:
 	velocity.x += Input.get_axis("move_left","move_right") * playerSpeed
 	velocity.x *= friction
 	if Input.is_action_just_pressed("dash"):
 		velocity.x+=Input.get_axis("move_left","move_right")*dashSpeed
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y=-jumpSpeed
 	velocity.y += gravity
 	move_and_slide()
