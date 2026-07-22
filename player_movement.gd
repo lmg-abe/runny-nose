a9cd734b255b104d920a8e5cd3e3bd6a5b49fa96
lmg-toad <toad@louisvillemakesgames.org>
2026-07-22 11:09:25 -0400

    added bubbles and goops

--- player_movement.gd
+++ player_movement.gd
@@ -20,6 +20,16 @@ var jumpAnim = load('res://runnyNoseJump.png')
 var fallAnim = load('res://runnyNoseFall.png')
 var walkAnim : int = 0
 
+func _ready() -> void:
+	Global.update_goops.connect(_on_update_goops)
+	Global.update_bubbles.connect(_on_update_bubbles)
+
+func _on_update_goops() :
+	canSneeze = true
+
+func  _on_update_bubbles():
+	canDash = true
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_left"): facingDirection = -1
 	if Input.is_action_pressed("move_right"): facingDirection = 1
