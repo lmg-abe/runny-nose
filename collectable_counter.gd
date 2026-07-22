d5fe8227328b9c34e8579e48b9b4229dd194cab7
lmg-abe <abe@louisvillemakesgames.org>
2026-07-22 10:54:20 -0400

    collectables (they're named)
    cells

--- collectable_counter.gd
+++ collectable_counter.gd
@@ -3,7 +3,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = "Unnamed Collectables: " + str(Global.collectables)
+	text = "Cells: " + str(Global.collectables)
 	Global.update_collectables.connect(_on_update_collectables)
 func _on_update_collectables():
-	text = "Unnamed Collectables: " + str(Global.collectables)
+	text = "Cells: " + str(Global.collectables)
