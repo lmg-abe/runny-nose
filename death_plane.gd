bf1608cd358b44ab8f107f6f91fc8e09c641bb2c
lmg-abe <abe@louisvillemakesgames.org>
2026-07-23 10:57:41 -0400

    enemies imported from personal proj

--- death_plane.gd
+++ death_plane.gd
@@ -2,5 +2,6 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	Global.deaths += 1
-	get_tree().reload_current_scene.call_deferred()
+	if body.name == "Runny":
+		Global.deaths += 1
+		get_tree().reload_current_scene.call_deferred()
