7dac47981ebdda7cde3a5baf3c006ca30b98bb90
Earthmars643 <xandermeurer@gmail.com>
2026-07-22 20:16:34 -0400

    fixed button bug
    button no longer checks based on bodies entering & exiting but instead if there is a body on it. it makes sense now

--- button.gd
+++ button.gd
@@ -2,11 +2,20 @@ extends Area2D
 
 @export var doorObj : String = "Door"
 
-func _on_body_entered(body: Node2D) -> void:
-	var node = get_parent().get_node(doorObj)
-	node.open()
+#func _on_body_entered(body: Node2D) -> void:
+	#var node = get_parent().get_node(doorObj)
+	#node.open()
+#
+#
+#func _on_body_exited(body: Node2D) -> void:
+	#var node = get_parent().get_node(doorObj)
+	#node.close()
 
-
-func _on_body_exited(body: Node2D) -> void:
+func _process(delta: float) -> void:
+	var stuffOnButton = self.get_overlapping_bodies()
 	var node = get_parent().get_node(doorObj)
-	node.close()
+	for body in stuffOnButton:
+		if body is RigidBody2D or body is CharacterBody2D:
+			node.open()
+	if len(stuffOnButton)==0:
+		node.close()
