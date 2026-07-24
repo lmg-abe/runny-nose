7a3c4b76d393f807045a52c77a22d04ba3cf6ddd
lmg-abe <abe@louisvillemakesgames.org>
2026-07-24 16:00:19 -0400

    final fixes

--- return_to_title.gd
+++ return_to_title.gd
@@ -4,4 +4,4 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://area_1.tscn")
+	get_tree().change_scene_to_file("res://menu.tscn")
--- scoreCalculator.gd
+++ scoreCalculator.gd
@@ -8,7 +8,7 @@ var yourRank : int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	score = ((Global.collectables) / (Global.deaths+1.0)) * 100
-	text = "\nYour Score: "+str(int(score)) + "\nRank: [unimplimented]"
+	text = "\nYour Score: "+str(int(score))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
