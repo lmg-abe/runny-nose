00c32ce694c77c3d2be6e1b27cf9e52fbe4acaa8
Earthmars643 <xandermeurer@gmail.com>
2026-07-23 21:47:29 -0400

    gave bubbles & goop actual visuals
    also added vertical parallax

--- bubble.gd
+++ bubble.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+var animTimer : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +8,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	animTimer+=1
+	get_node("Icon").position = Vector2(0,sin((deg_to_rad(animTimer*5)))*8)
+
 
 
 func _on_body_entered(body: Node2D) -> void:
--- do paralax.gd
+++ do paralax.gd
@@ -3,4 +3,5 @@ extends Sprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	set_position(Vector2((-get_parent().position.x) / 10, 0))
+	#set_position(Vector2((-get_parent().position.x) / 10, 0))
+	set_position((-get_parent().position) / 20)
